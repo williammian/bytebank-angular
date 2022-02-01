c104dd0a5d950d0d5c3880d2fdd95a1a40b329a2
William Mian <william_mian@yahoo.com.br>
2022-02-01 10:38:28 -0300

    property bind

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+
+  valor: number;
+  destino: number;
+
   transferir() {
     console.log('Solicitada nova transferencia');
+    console.log('Valor: ', this.valor);
+    console.log('Destino: ', this.destino);
   }
 }
