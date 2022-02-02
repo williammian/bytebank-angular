2074ce1930befd6599a02fb748d86ca2a556845e
William Mian <william_mian@yahoo.com.br>
2022-02-02 14:28:58 -0300

    integrando os extratos com a API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
@@ -18,7 +19,8 @@ registerLocaleData(localePt, 'pt');
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt'},
--- src/app/services/transferencia.service.ts
+++ src/app/services/transferencia.service.ts
@@ -1,18 +1,26 @@
+import { Transferencia } from './../models/transferencia.model';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
 
+  private url = 'http://localhost:3000/transferencias';
   private listaTransferencia: any[];
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.listaTransferencia = [];
   }
 
   get transferencias() {
     return this.listaTransferencia;
+  }
+
+  todas(): Observable<Transferencia[]> {
+    return this.httpClient.get<Transferencia[]>(this.url);
   }
 
   adicionar(transferencia: any) {
